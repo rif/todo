262065a810d2c54f4a8ac607b77aeada9b995051
Radu Ioan Fericean <radu@fericean.ro>
2012-08-01 14:57:51 +0300

    started to write tests

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,9 +1,14 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TasksTest(TestCase):
+    def test_login(self):
+        response = self.client.post(reverse('userena_signin'), {'username': 'rif', 'password': 'test'})
+        self.assertEqual(response.status_code, 200)
+         
+    def test_incr(self):
+        logged_in = self.client.login(username='rif', password='test')
+        self.assertTrue(logged_in)
+        t1 = Task.objects.all()[0]
+        self.assertEqual(t1.priority,2)
+        
